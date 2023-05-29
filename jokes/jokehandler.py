9bebd68cd87396f0be44be3673f9098325c14248
Joeljac0 <jacobsson.joel5@gmail.com>
2023-05-29 14:46:51 +0200

    joke
    joke

--- jokes/jokehandler.py
+++ jokes/jokehandler.py
@@ -1,13 +1,18 @@
 import requests
+import datetime
 
 class Jokehandler:
 
     def __init__(self, adress):
         self.adress = adress
 
+        
     def get_joke(self):
         req = requests.get(self.adress)
         json_data = req.json()
         joke = json_data['joke']
+
+        nu = datetime.datetime.now()
+        #print(f"Construktor körs tid: {nu}")
 
         return joke
