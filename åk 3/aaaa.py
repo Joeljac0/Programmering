db643898e80b58b3afe0864c5d091ad6ebc420e7
Joeljac0 <jacobsson.joel5@gmail.com>
2023-09-15 12:46:27 +0200

    finally
    yes

--- åk 3/aaaa.py
+++ åk 3/aaaa.py
@@ -1,4 +1,6 @@
 import ikea
+import os
+import time
 
 looping = True
 
@@ -7,13 +9,15 @@ Skotterud = ikea.ikea("Skotterud", "resårbotten", 4295)
 
 
 list = [Billy, Skotterud]
-
+saldo = 5000
 #for ikea in list:
 #    print(ikea.price)
 
 while looping:
+    #os.system('cls' if os.name == 'nt' else 'clear')
     print("(1) Lista av Möbler")
-    print("(2) Avsluta")
+    print("(2) Visa ditt saldo")
+    print("(3) Avsluta")
     choice = input("Val: ")
     if choice == "1":
         print("Möbler")
@@ -28,19 +32,73 @@ while looping:
         schoice = input("Val: ")
 
         if schoice == "1":
-            for Billy in list:
-                print(ikea.name)
-                print (ikea.price)
+            print(Billy.name)
+            print(Billy.category)
+            print(str(Billy.price) + " kr")
+            print("You have the money of: " + str(saldo) + " kr, " + "do you want buy?")
+            bchoice = input("j/n: ")
+            if bchoice == "j":
+                if saldo > Billy.price:
+                    print("Du bra val, " + Billy.name + " köpad")
+                    saldo = saldo - Billy.price
+                    print("Du innehavar nu " + str(saldo) + " peng.")
+                else:
+                    print("Jävla fattiglapp, gå tillbaka till ditt hus oh wait du har inget.")
+                    print()
+                    time.sleep(5)
+                    print("No money having ass")
+                    time.sleep(3)
+            elif bchoice == "n":
+                print("Fuck you")
+                print("restart")
+                restart = input("j/n: ")
+                if restart == "j":
+                    print("good")
+                else:
+                    print()
+                    time.sleep(2)
+                    print("...Go then")
+                    print()
+                    time.sleep(2)
+                    break
 
         elif schoice == "2":
-            for skotterud in list:
-                print(ikea.name)
-                print(ikea.price)
-
+            print(Skotterud.name)
+            print(Skotterud.category)
+            print(str(Skotterud.price) + " kr")
+            print("You have the money of: " + str(saldo) + " kr, " + "do you want buy?")
+            skchoice = input("j/n: ")
+            if skchoice == "j":
+                if saldo > Skotterud.price:
+                    print("Du bra val, " + Skotterud.name + " köpad")
+                    saldo = saldo - Skotterud.price
+                    print("Du innehavar nu " + str(saldo) + " peng.")
+                else:
+                    print("Jävla fattiglapp, gå tillbaka till ditt hus oh wait du har inget.")
+                    print()
+                    time.sleep(5)
+                    print("No money having ass")
+                    time.sleep(3)
+            elif skchoice == "n":
+                print("Fuck you")
+                print("restart")
+                restart = input("j/n: ")
+                if restart == "j":
+                    print("good")
+                else:
+                    print()
+                    time.sleep(2)
+                    print("...Go then")
+                    print()
+                    time.sleep(2)
+                    break
                 
         
     elif choice == "2":
+        print(str(saldo))
+
+    elif choice == "3":
         looping = False
 
     else:
-        print("invalid choice")
+        print("Sug mina bollar")
